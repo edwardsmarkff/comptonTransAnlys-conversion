257b72d854d3d69a249467bec70aefbf2aa4f928
marky edwards <mark@edwardsmark.com>
2021-06-14 16:02:45 -0700

    Update sql-calls.sql

--- sql-calls.sql
+++ sql-calls.sql
@@ -15,6 +15,7 @@
 
 
 
+	
 		SELECT	ALL ''
 		,	stimwordPosition.stimwordPositionWord			'StimwordPositionWord'
 		,	stimwordPosition.stimwordPositionName			'stimwordPositionName'
@@ -31,14 +32,14 @@
 		,	stimwordPosition.stimwordPositionPageNbr		'Eval_masterPageNbr'
 		,	stimwordPosition.stimwordPositionAutoIncr		'Eval_masterAutoIncr'
 
-		,	clientStimwordCURRENT.clientStimwordError1		'clientStimwordError1_current'
-		,	clientStimwordCURRENT.clientStimwordError2		'clientStimwordError2_current'
-		,	clientStimwordCURRENT.clientStimwordError3		'clientStimwordError3_current'
-		,	clientStimwordCURRENT.clientStimwordError4		'clientStimwordError4_current'
-		,	clientStimwordREPLICATE.clientStimwordError1		'clientStimwordError1_replicate'
-		,	clientStimwordREPLICATE.clientStimwordError2		'clientStimwordError2_replicate'
-		,	clientStimwordREPLICATE.clientStimwordError3		'clientStimwordError3_replicate'
-		,	clientStimwordREPLICATE.clientStimwordError4		'clientStimwordError4_replicate'
+		,	clientStimwordCURRENT.clientContextError1		'clientContextError1_current'
+		,	clientStimwordCURRENT.clientContextError2		'clientContextError2_current'
+		,	clientStimwordCURRENT.clientContextError3		'clientContextError3_current'
+		,	clientStimwordCURRENT.clientContextError4		'clientContextError4_current'
+		,	clientStimwordREPLICATE.clientContextError1		'clientContextError1_replicate'
+		,	clientStimwordREPLICATE.clientContextError2		'clientContextError2_replicate'
+		,	clientStimwordREPLICATE.clientContextError3		'clientContextError3_replicate'
+		,	clientStimwordREPLICATE.clientContextError4		'clientContextError4_replicate'
 
 		FROM	stimwordPosition
 		LEFT OUTER JOIN clientStimword clientStimwordCURRENT ON
@@ -47,10 +48,12 @@
 		AND	stimwordPosition.stimwordPositionPageNbr	=	clientStimwordCURRENT.clientStimwordPageNbr
 		AND	stimwordPosition.stimwordPositionLineNbr	=	clientStimwordCURRENT.clientStimwordLineNbr
 		AND	stimwordPosition.stimwordPositionWord 		=	clientStimwordCURRENT.clientStimwordWord
-		AND	stimwordPosition.stimwordPositionName		=	clientStimwordCURRENT.clientStimwordName
-		AND	stimwordPosition.stimwordPositionNbr		=	clientStimwordCURRENT.clientStimwordNbr
+
+		AND	stimwordPosition.stimwordPositionName		=	clientStimwordCURRENT.clientContextPosition
+		AND	stimwordPosition.stimwordPositionNbr		=	clientStimwordCURRENT.clientStimwordPositionNbr
 		AND	stimwordPosition.stimwordPositionSetting	=	clientStimwordCURRENT.clientStimwordSetting
-		AND	stimwordPosition.stimwordPositionPhoneme	=	clientStimwordCURRENT.clientStimwordPhoneme	
+
+		AND	stimwordPosition.stimwordPositionPhoneme	=	clientStimwordCURRENT.clientContextPhoneme	
 				## AND	stimwordPosition.stimwordPosition_target_sound_1	=	clientStimwordCURRENT.clientStimword_target_sound_1	
 				## AND	stimwordPosition.stimwordPosition_target_sound_2	=	clientStimwordCURRENT.clientStimword_target_sound_2	
 				## AND	stimwordPosition.stimwordPosition_target_sound_3	=	clientStimwordCURRENT.clientStimword_target_sound_3	
@@ -62,27 +65,30 @@
 		)
 		LEFT OUTER JOIN clientStimword clientStimwordREPLICATE ON
 		(	1
-		AND	stimwordPosition.stimwordPositionLayoutName 	=	clientStimwordREPLICATE.clientStimwordLayoutName
+		AND	stimwordPosition.stimwordPositionLayoutName 	=	clientStimwordREPLICATE.clientMasterLayoutName
 		AND	stimwordPosition.stimwordPositionPageNbr 	=	clientStimwordREPLICATE.clientStimwordPageNbr
 		AND	stimwordPosition.stimwordPositionLineNbr	=	clientStimwordREPLICATE.clientStimwordLineNbr
 		AND	stimwordPosition.stimwordPositionWord 		=	clientStimwordREPLICATE.clientStimwordWord
-		AND	stimwordPosition.stimwordPositionName		=	clientStimwordREPLICATE.clientStimwordName
-		AND	stimwordPosition.stimwordPositionNbr		=	clientStimwordREPLICATE.clientStimwordNbr
+
+		AND	stimwordPosition.stimwordPositionName		=	clientStimwordREPLICATE.clientContextPosition
+		AND	stimwordPosition.stimwordPositionNbr		=	clientStimwordREPLICATE.clientStimwordPositionNbr
 		AND	stimwordPosition.stimwordPositionSetting	=	clientStimwordREPLICATE.clientStimwordSetting
-		AND	stimwordPosition.stimwordPositionPhoneme	=	clientStimwordREPLICATE.clientStimwordPhoneme	
+
+		AND	stimwordPosition.stimwordPositionPhoneme	=	clientStimwordREPLICATE.clientContextPhoneme	
 				## AND	stimwordPosition.stimwordPosition_target_sound_1	=	clientStimwordREPLICATE.clientStimword_target_sound_1	
 				## AND	stimwordPosition.stimwordPosition_target_sound_2	=	clientStimwordREPLICATE.clientStimword_target_sound_2	
 				## AND	stimwordPosition.stimwordPosition_target_sound_3	=	clientStimwordREPLICATE.clientStimword_target_sound_3	
 
 		AND	clientStimwordREPLICATE.teacherEmail		=	'info@englishwithoutaccent.com'
-		AND	clientStimwordREPLICATE.clientMasterrEmail	=	'mark@edwardsmark.com'
+		AND	clientStimwordREPLICATE.clientMasterEmail	=	'mark@edwardsmark.com'
 		AND	clientStimwordREPLICATE.clientMasterSessionName	=	'Time1'
-		AND	clientStimwordREPLICATE.clientMastrdLayoutName	=	'P-ESL'
+		AND	clientStimwordREPLICATE.clientMasterLayoutName	=	'P-ESL'
 		)
 		WHERE	1			/* dummy first one */
 		AND	stimwordPosition.stimwordPositionLayoutName				=	'P-ESL'
 		AND	stimwordPosition.stimwordPositionPageNbr				=	1
 		AND	stimwordPosition.stimwordPositionLineNbr				=	1
-		AND	stimwordPosition.stimwordPosition_stimulus_word				=	'Horse'
+		AND	stimwordPosition.stimwordPositionWord					=	'Horse'
 		ORDER BY	stimwordPosition.stimwordPositionAutoIncr
 		;
+
